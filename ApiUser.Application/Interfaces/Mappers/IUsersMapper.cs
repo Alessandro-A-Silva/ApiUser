f124287b53d9020f065e1d77239b8a4d6dbbed75
Alessandro Silva <alessandro-almeida-silva@hotmail.com>
2024-01-24 21:26:29 -0300

    feat: Application

---
+++ ApiUser.Application/Interfaces/Mappers/IUsersMapper.cs
@@ -0,0 +1,17 @@
+﻿using ApiUser.Application.Dtos;
+using ApiUser.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiUser.Application.Interfaces.Mappers
+{
+    public interface IUsersMapper
+    {
+        Users DtoToEntity(UsersDto dto);
+        UsersDto EntityToDto(Users entity);
+        IEnumerable<UsersDto> ListEntityToListDto(IEnumerable<Users> entities);
+    }
+}
---
+++ ApiUser.Application/Mappers/UsersMapper.cs
@@ -0,0 +1,44 @@
+﻿using ApiUser.Application.Dtos;
+using ApiUser.Application.Interfaces.Mappers;
+using ApiUser.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiUser.Application.Mappers
+{
+    public class UsersMapper : IUsersMapper
+    {
+        public Users DtoToEntity(UsersDto dto)
+        {
+            return new Users()
+            {
+                Id = dto.Id,
+                FirstName = dto.FirstName,
+                LastName = dto.LastName,
+            };
+        }
+
+        public UsersDto EntityToDto(Users entity)
+        {
+            return new UsersDto()
+            {
+                Id = entity.Id,
+                FirstName = entity.FirstName,
+                LastName = entity.LastName
+            };
+        }
+
+        public IEnumerable<UsersDto> ListEntityToListDto(IEnumerable<Users> entities)
+        {
+            return entities.Select(x => new UsersDto()
+            {
+                Id = x.Id,
+                FirstName = x.FirstName,
+                LastName = x.LastName,
+            });
+        }
+    }
+}
