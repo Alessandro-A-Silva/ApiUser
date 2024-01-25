a4b5ce241140f89d0e52ab93d2008a30f556d4d8
Alessandro Silva <alessandro-almeida-silva@hotmail.com>
2024-01-25 00:16:31 -0300

    feat Presentation

---
+++ ApiUser/Controllers/UsersController.cs
@@ -0,0 +1,92 @@
+﻿using ApiUser.Application.Interfaces.ApplicationServices;
+using ApiUser.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace ApiUser.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUsersApplicationService _usersApplicationService;
+        public UsersController(IUsersApplicationService usersApplicationService)
+        {
+            _usersApplicationService = usersApplicationService;
+        }
+
+        [HttpPost]
+        [SwaggerOperation(Summary = "Register new user.")]
+        [SwaggerResponse(StatusCodes.Status200OK)]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Create([FromBody] Users request)
+        {
+            try
+            {
+                if (await _usersApplicationService.Create(request))
+                    return StatusCode(StatusCodes.Status201Created, "Uregistered successfully.");
+
+                return StatusCode(StatusCodes.Status400BadRequest, "Already registered user.");
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error when trying to register." + ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [SwaggerOperation(Summary = "Read all users.")]
+        [SwaggerResponse(StatusCodes.Status200OK)]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> ReadAll([FromQuery] Users request)
+        {
+            try
+            {
+                return Ok(await _usersApplicationService.ReadAll(request));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error when trying to read users." + ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        [SwaggerOperation(Summary = "Delete a user.")]
+        [SwaggerResponse(StatusCodes.Status200OK)]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Delete([FromBody] Users request)
+        {
+            try
+            {
+                if (await _usersApplicationService.Delete(request))
+                    return StatusCode(StatusCodes.Status200OK, "Deleted successfully.");
+
+                return StatusCode(StatusCodes.Status404NotFound, "User not found.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error when trying to delete." + ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [SwaggerOperation(Summary = "Update a user.")]
+        [SwaggerResponse(StatusCodes.Status200OK)]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Update([FromBody] Users request)
+        {
+            try
+            {
+                if (await _usersApplicationService.Update(request))
+                    return StatusCode(StatusCodes.Status200OK, "Updated successfully.");
+
+                return StatusCode(StatusCodes.Status404NotFound, "User not found.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error when trying to register." + ex.Message);
+            }
+        }
+    }
+}
--- ApiUser/Program.cs
+++ ApiUser/Program.cs
@@ -1,5 +1,10 @@
+using ApiUser.Infrastructure.CrossCutting.IOC;
 using ApiUser.Infrastructure.Data;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,8 +13,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options => options.SwaggerDoc("v1", new OpenApiInfo
+{
+    Version = "v1",
+    Title = "User API by Alessandro Silva.",
+    Description = "An .NET 6 Web API for managing user."
+}));
+
 builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("DB"));
+
+builder.Services.AddCors(c => c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
+
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+
+builder.Host.ConfigureContainer<ContainerBuilder>(builder => builder.RegisterModule(new ModuleIOC()));
 
 var app = builder.Build();
 
